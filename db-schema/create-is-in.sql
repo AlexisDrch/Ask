1c00593e4bb490581741c09d2331c2619de2d782
Alexis Durocher <aleksi.durocher@wanadoo.fr>
2018-02-26 19:57:29 +0100

    first schema - not tested yet on postgres

---
+++ db-schema/create-is-in.sql
@@ -0,0 +1,6 @@
+CREATE TABLE IF NOT EXISTS "is_in" (
+	category_id int NOT NULL REFERENCES category(category_id) ON DELETE CASCADE,
+	item_id int NOT NULL REFERENCES category(item_id) ON DELETE CASCADE
+);
+
+
