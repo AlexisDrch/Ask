13e288f509d7d4bf0d77b6ab212e12a0a0f6f704
Alexander Fache <alexander.fache@gmail.com>
2018-02-26 20:35:54 +0100

    Added starter classes and initial structure.

--- Android/app/src/main/java/Item.java
+++ Android/app/src/main/java/Item.java
@@ -1,10 +1,10 @@
 import java.net.URL;
+import java.util.LinkedList;
 
 /**
  * Created by alexander on 2/26/2018.
  *
- *
- *
+ * Item class to hold information for each individual item.
  *
  */
 
@@ -12,11 +12,64 @@ public class Item {
 
     private String uuid;
     private String name;
-    private double averagePrice;
-    private URL image;
+    private LinkedList<String> tags;
+    private double price;
+    private URL itemImage;
 
-    // stayayaa
-    //name, picture, average price, id
+    public Item(String uuid, String name, LinkedList<String> tags, double price, URL itemImage) {
+        this.uuid = uuid;
+        this.name = name;
+        this.tags = tags;
+        this.price = price;
+        this.itemImage = itemImage;
+    }
 
+    public String getUuid() {
+        return uuid;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public LinkedList<String> getTags() {
+        return this.tags;
+    }
+
+    public boolean addTag(String newTag) {
+        return this.tags.add(newTag);
+    }
+
+    public boolean removeTag(String tag) {
+        return this.tags.remove(tag);
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public URL getItemImage() {
+        return itemImage;
+    }
+
+    public void setItemImage(URL itemImage) {
+        this.itemImage = itemImage;
+    }
+
+    @Override
+    public String toString() {
+        return "Item{" +
+                "uuid='" + uuid + '\'' +
+                ", name='" + name + '\'' +
+                '}';
+    }
 
 }
--- Android/app/src/main/java/Request.java
+++ Android/app/src/main/java/Request.java
@@ -1,12 +1,94 @@
+import java.util.LinkedList;
+
 /**
  * Created by alexander on 2/26/2018.
  *
- * Linkes user and item together
+ * Links requests and offers together.
  *
  */
 
-
-
 public class Request {
+
+    private User requester;
+    private Item itemRequesting;
+    private String beginDate;
+    private String endDate;
+    private String description;
+
+    private int DELETED = -1;
+    private int IN_PROGRESS = 0;
+    private int OFFERS_PENDING = 1;
+    private int MATCHED = 2;
+    private int status;
+
+    private LinkedList<Offer> offers;
+    private User matcher;
+
+    public Request(User requester, Item itemRequesting, String beginDate, String endDate, String description) {
+        this.requester = requester;
+        this.itemRequesting = itemRequesting;
+        this.beginDate = beginDate;
+        this.endDate = endDate;
+        this.description = description;
+        this.status = IN_PROGRESS;
+        this.offers = new LinkedList<>();
+        this.matcher = null;
+    }
+
+    public User getRequester() {
+        return requester;
+    }
+
+    public Item getItem() {
+        return itemRequesting;
+    }
+
+    public String getBeginDate() {
+        return beginDate;
+    }
+
+    public String getEndDate() {
+        return endDate;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public int getStatus() {
+        return status;
+    }
+
+    public void setStatus(int status) {
+        this.status = status;
+    }
+
+    public LinkedList<Offer> getOffers() {
+        return offers;
+    }
+
+    public User getMatcher() {
+        return matcher;
+    }
+
+    /**
+     *
+     * @param newOffer
+     * @return true of successful add, false if unsuccessful add
+     */
+    public boolean addOffer(Offer newOffer) {
+        return offers.add(newOffer);
+    }
+
+    @Override
+    public String toString() {
+        return "Request{" +
+                "requester=" + requester +
+                ", item=" + itemRequesting +
+                ", beginDate='" + beginDate + '\'' +
+                ", endDate='" + endDate + '\'' +
+                ", status='" + status + '\'' +
+                '}';
+    }
 
 }
