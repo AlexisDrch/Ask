43689edb787b9b70df4c33aca74daf439050ea12
Alexis Durocher <aleksi.durocher@wanadoo.fr>
2018-02-27 18:42:47 +0100

    test on db : worked fine

--- db-schema/create-offer.sql
+++ db-schema/create-offer.sql
@@ -2,7 +2,7 @@ CREATE TABLE IF NOT EXISTS "offer" (
 	offer_id serial NOT NULL PRIMARY KEY,
 	belonging_id int NOT NULL REFERENCES belonging(belonging_id) ON DELETE CASCADE,
 	request_id int NOT NULL REFERENCES request(request_id) ON DELETE CASCADE,
-	provider_id int NOT NULL REFERENCES user(user_id) ON DELETE CASCADE,
+	provider_id int NOT NULL REFERENCES "user"(user_id) ON DELETE CASCADE,
 	begin_date date NOT NULL,
 	end_date date NOT NULL,
 	lon double precision NOT NULL,
@@ -11,5 +11,5 @@ CREATE TABLE IF NOT EXISTS "offer" (
 	message varchar(1000) NOT NULL
 );
 
-# offers are made by providers. They correspond to one item requested.
-# @todo : ON DELETE CASCADE necessary / dangerous ?
+--offers are made by providers. They correspond to one item requested.
+--@todo : ON DELETE CASCADE necessary / dangerous ?
--- db-schema/create-request.sql
+++ db-schema/create-request.sql
@@ -1,8 +1,8 @@
 CREATE TABLE IF NOT EXISTS "request" (
 	request_id serial NOT NULL PRIMARY KEY,
 	item_id int NOT NULL REFERENCES item(item_id) ON DELETE CASCADE,
-	requester_id int NOT NULL REFERENCES user(user_id) ON DELETE CASCADE,
-	provider_id int NOT NULL REFERENCES user(user_id) ON DELETE CASCADE,
+	requester_id int NOT NULL REFERENCES "user"(user_id) ON DELETE CASCADE,
+	provider_id int NOT NULL REFERENCES "user"(user_id) ON DELETE CASCADE,
 	begin_date date NOT NULL,
 	end_date date NOT NULL,
 	lon double precision NOT NULL,
@@ -10,5 +10,5 @@ CREATE TABLE IF NOT EXISTS "request" (
 	description varchar(1000) NOT NULL
 );
 
-# requests are made by requesters. They correspond to one item requested.
-# @todo : ON DELETE CASCADE necessary / dangerous ?
+--requests are made by requesters. They correspond to one item requested.
+--@todo : ON DELETE CASCADE necessary / dangerous ?
