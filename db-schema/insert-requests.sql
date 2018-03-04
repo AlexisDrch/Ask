c3494abd5dc5173382b1167e8e9a5e220bacd05f
Alexis Durocher <aleksi.durocher@wanadoo.fr>
2018-03-04 21:46:46 +0100

    add insert request

---
+++ db-schema/insert-requests.sql
@@ -0,0 +1,5 @@
+INSERT INTO request (item_id, requester_id, provider_id, begin_date, end_date, lon, lat, description)
+  VALUES (1, 1, 2, '2018-12-17', '2018-12-17',
+  	-122.431297, 37.773972, 'Looking for a board 6.8 near SF. Can move to pick up !');
+
+
