fa4005b716412803fcafcf1bdf8f4807c3b65124
Alexis Durocher <aleksi.durocher@wanadoo.fr>
2018-03-12 18:40:46 +0100

    add deploy url

--- db-connection.js
+++ db-connection.js
@@ -5,8 +5,11 @@ var options = {
   promiseLib: promise
 };
 
+const LOCALHOST_URL = 'postgres://localhost:5432/ask'
+const DEPLOYMENT_URL = 'postgres://ggqwespmozubup:547149f9860bb143956959fe6fbb22bcf43857b9a2de9e8312049bb3ba82190e@ec2-174-129-26-203.compute-1.amazonaws.com:5432/daqoghiq44sdjd:5432/ask'
+
 var pgp = require('pg-promise')(options);
-var connectionString = 'postgres://localhost:5432/ask';
+var connectionString = DEPLOYMENT_URL;
 var db = pgp(connectionString);
 
 
