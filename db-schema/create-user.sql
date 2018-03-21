f4dddb7c49aebc8b85f8c94cefa292753505f641
Alexis Durocher <aleksi.durocher@wanadoo.fr>
2018-03-21 20:31:48 +0100

    add login

--- db-schema/create-user.sql
+++ db-schema/create-user.sql
@@ -8,10 +8,10 @@ CREATE TABLE IF NOT EXISTS "user" (
 	description varchar(1000) DEFAULT NULL,
 	ppicture_url varchar(1000) NOT NULL,
 	phone_num varchar(500) DEFAULT NULL,
-	address varchar(500) DEFAULT NULL,
 	age int DEFAULT NULL,
 	sex int DEFAULT NULL,
 	country int DEFAULT NULL,
+	address varchar(1000) DEFAULT NULL,
 	address_string varchar(1000) DEFAULT NULL,
 	UNIQUE(user_name, password),
 	UNIQUE(user_name, email)
