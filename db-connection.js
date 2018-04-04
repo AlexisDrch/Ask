11c422a14b0fc76824a2ae0e58d83ea729341639
Alexis Durocher <aleksi.durocher@wanadoo.fr>
2018-04-04 17:59:47 +0200

    add pictures and desc

--- db-connection.js
+++ db-connection.js
@@ -9,7 +9,7 @@ const LOCALHOST_URL = 'postgres://localhost:5432/ask'
 const DEPLOYMENT_URL = 'postgres://ggqwespmozubup:547149f9860bb143956959fe6fbb22bcf43857b9a2de9e8312049bb3ba82190e@ec2-174-129-26-203.compute-1.amazonaws.com:5432/daqoghiq44sdjd'
 
 var pgp = require('pg-promise')(options);
-var connectionString = LOCALHOST_URL;
+var connectionString = DEPLOYMENT_URL;
 var db = pgp(connectionString);
 
 
