d1979a5dfe86fe70c2a18b7c59e05e03a965fef8
Alexis Durocher <aleksi.durocher@wanadoo.fr>
2018-04-06 11:47:09 +0200

    actual price be used for the request

--- Android/app/src/main/java/com/ask/ask/RecyclerViewAdapter.java
+++ Android/app/src/main/java/com/ask/ask/RecyclerViewAdapter.java
@@ -77,7 +77,7 @@ public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapte
         final Item currentItem = LocalData.getHashMapItemsById().get(currentRequest.getItem_id());
         holder.itemName.setText(currentItem.getName());
         holder.itemIcon.setImageResource(currentItem.getIcon());
-        final String price = String.valueOf(currentItem.getPrice());
+        final String price = String.valueOf(currentRequest.getRequest_price());
         holder.itemPrice.setText(price);
 
         // Match button
