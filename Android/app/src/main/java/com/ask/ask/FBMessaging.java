a90ea3903a4f6b9578f6e696cf3b1ee3ef498b68
carolyn <cs947@cornell.edu>
2018-04-09 18:14:29 +0200

    hmm

---
+++ Android/app/src/main/java/com/ask/ask/FBMessaging.java
@@ -0,0 +1,24 @@
+package com.ask.ask;
+
+import android.util.Log;
+
+import com.google.firebase.iid.FirebaseInstanceId;
+import com.google.firebase.iid.FirebaseInstanceIdService;
+
+public class FBMessaging extends FirebaseInstanceIdService {
+
+    @Override
+    public void onTokenRefresh() {
+        // Get updated InstanceID token.
+        String refreshedToken = FirebaseInstanceId.getInstance().getToken();
+        Log.d("refreshingtoken", "Refreshed token: " + refreshedToken);
+
+        // If you want to send messages to this application instance or
+        // manage this apps subscriptions on the server side, send the
+        // Instance ID token to your app server.
+        sendRegistrationToServer(refreshedToken);
+    }
+
+    private void sendRegistrationToServer(String refreshedToken) {
+    }
+}
